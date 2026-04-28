4c87bfc112494e3ae141d1ea3bed8acebf04683d
kritik gianta <149412963+kritikGianta@users.noreply.github.com>
2026-04-28 12:13:47 +0530

    docs: auto-document src/components/Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,53 @@
-// src/components/Navbar.jsx
 import React from 'react';
 
+/**
+ * Navbar component that renders the navigation bar for the application.
+ * 
+ * @returns {JSX.Element} The JSX element representing the navigation bar.
+ */
 function Navbar() {
   return (
+    /**
+     * The navigation bar container element.
+     */
     <nav className="navbar">
+      {/**
+       * The container element for the navigation bar content.
+       */}
       <div className="navbar-container">
+        {/**
+         * The brand link for the application.
+         */}
         <a href="#top" className="navbar-brand">SkillPath Tracker</a>
+        {/**
+         * The list of navigation links.
+         */}
         <ul className="navbar-links">
-          <li><a href="#add-roadmap" className="navbar-link">Add Roadmap</a></li>
-          <li><a href="#your-roadmaps" className="navbar-link">Your Roadmaps</a></li>
+          {/**
+           * The list item for the "Add Roadmap" link.
+           */}
+          <li>
+            {/**
+             * The link to the "Add Roadmap" section.
+             */}
+            <a href="#add-roadmap" className="navbar-link">Add Roadmap</a>
+          </li>
+          {/**
+           * The list item for the "Your Roadmaps" link.
+           */}
+          <li>
+            {/**
+             * The link to the "Your Roadmaps" section.
+             */}
+            <a href="#your-roadmaps" className="navbar-link">Your Roadmaps</a>
+          </li>
         </ul>
       </div>
     </nav>
   );
 }
 
+/**
+ * Exports the Navbar component as the default export.
+ */
 export default Navbar;
